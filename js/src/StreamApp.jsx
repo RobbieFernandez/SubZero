abc4991c3a351b81c57ee2e251a173b37ae4289c
Robert Fernandez <fernandez.robert.97@gmail.com>
2019-08-30 21:34:01 +0800

    Added play button. Required by chrome

--- js/src/StreamApp.jsx
+++ js/src/StreamApp.jsx
@@ -2,19 +2,34 @@ import {AudioVisualiser} from './AudioVisualiser'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function StreamApp(props) {
-    return <div>
-        <h1>{"Stream"}</h1>
-        <AudioVisualiser
-            audioStreamUrl={props.audioStreamUrl}
-            canvasWidth={props.visualiserWidth}
-            canvasHeight={props.visualiserHeight}
-            numBars={100}
-            barGap={2}
-            barColour={"#9933ff"}
-            autoPlay={true}
-        />
-    </div>
+class StreamApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {"playing": false};
+    }
+
+    startPlaying() {
+        this.setState({"playing": true});
+    }
+
+    render() {
+        return <div>
+            <h1>{"Stream"}</h1>
+            {
+                this.state.playing ?
+                <AudioVisualiser
+                    audioStreamUrl={this.props.audioStreamUrl}
+                    canvasWidth={this.props.visualiserWidth}
+                    canvasHeight={this.props.visualiserHeight}
+                    numBars={100}
+                    barGap={2}
+                    barColour={"#9933ff"}
+                    autoPlay={true}
+                /> :
+                <button onClick={this.startPlaying.bind(this)}>Play</button>
+            }
+        </div>
+    }
 }
 
 export function initStreamApp(audioStreamUrl, visualiserWidth, visualiserHeight, containerId) {
